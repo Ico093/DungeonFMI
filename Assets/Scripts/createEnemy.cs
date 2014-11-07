9313364e4e570bd5b191d52130dce9c2b0438461
georgi <gesha_vn@gmail.com>
2014-11-08 00:54:44 +0200

    created enemy

---
+++ Assets/Scripts/createEnemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createEnemy : MonoBehaviour {
+
+	public GameObject zombie;
+	public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int rand = Random.Range (1, 1000);
+		if(rand>900) 
+			zombie = (GameObject) Instantiate (zombie, new Vector3 (0f, 3f, 0f), Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/enemyController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyController : MonoBehaviour {
+
+	public float chaseSpeed = 0.002f;                           // The nav mesh agent's speed when chasing.
+
+	public GameObject player;    // Reference to the last global sighting of the player.
+
+	
+	void Start () {
+		player=GameObject.Find("player");
+		}
+	
+	void Update ()
+	{
+		
+		Vector3 player_position=player.transform.position;
+		transform.position -= ((transform.position - player_position).normalized * chaseSpeed * Time.deltaTime);
+		Debug.Log (transform.position);
+		
+	
+	}
+
+
+	
+
+}
