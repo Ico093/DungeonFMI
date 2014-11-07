b028ca4f0dc890b811b5a3c6d76555ba4c9040e8
Viktor Markov <vikata3@gmail.com>
2014-11-07 23:58:05 +0200

    Added sprite positioning. ITS ALIVE

--- Assets/Scripts/playerControllerScr.cs
+++ Assets/Scripts/playerControllerScr.cs
@@ -5,15 +5,65 @@ public class playerControllerScr : MonoBehaviour {
 
 	public float movementSpeed;
 
+	SpriteRenderer sr;
+	public Sprite top;
+	public Sprite bottom;
+	public Sprite left;
+	public Sprite right;
+	public Sprite topLeft;
+	public Sprite topRight;
+	public Sprite bottomLeft;
+	public Sprite bottomRight;
+
+
+
 	void Start () 
 	{
-	
+		sr = GetComponent<SpriteRenderer>();
 	}
 
 	void Update () 
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal") ;
-		float moveVertical = Input.GetAxis ("Vertical") ;
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 direction = new Vector2 ();
 		transform.Translate (new Vector3(moveHorizontal, moveVertical, 0).normalized * movementSpeed * Time.deltaTime);
+		if (moveHorizontal == 0 && moveVertical > 0) {
+			Debug.Log("TOP");
+			direction.x = 0;
+			direction.y = 1;
+			sr.sprite = top;
+		} else if (moveHorizontal == 0 && moveVertical < 0) {
+			direction.x = 0;
+			direction.y = -1;
+			sr.sprite = bottom;
+		} else if (moveHorizontal > 0 && moveVertical == 0) {
+			direction.x = 1;
+			direction.y = 0;
+			sr.sprite = right;
+		} else if (moveHorizontal < 0 && moveVertical == 0) {
+			sr.sprite = left;
+			direction.x = -1;
+			direction.y = 0;
+		} else if (moveHorizontal > 0 && moveVertical > 0) {
+			direction.x = 1;
+			direction.y = 1;
+			sr.sprite = topRight;
+		} else if (moveHorizontal < 0 && moveVertical > 0) {
+			direction.x = -1;
+			direction.y = 1;
+			sr.sprite = topLeft;
+		} else if (moveHorizontal > 0 && moveVertical < 0) {
+			direction.x = 1;
+			direction.y = -1;
+			sr.sprite = bottomRight;
+		} else if (moveHorizontal < 0 && moveVertical < 0) {
+			direction.x = -1;
+			direction.y = -1;
+			sr.sprite = bottomLeft;
+		} else { 
+			Debug.Log(" default " );
+		}
 	}
+
 }
