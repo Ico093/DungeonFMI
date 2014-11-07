d381367963d8283ce5873dc4fff03385234aa5ef
FIlip Uzunov <filkata@gmail.com>
2014-11-07 21:09:50 +0200

    initial commit

---
+++ Assets/Scripts/playerControllerScr.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControllerScr : MonoBehaviour {
+
+	public float movementSpeed;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal") * movementSpeed * Time.deltaTime;
+		float moveVertical = Input.GetAxis ("Vertical") * movementSpeed * Time.deltaTime;
+		transform.Translate (new Vector3(moveHorizontal, moveVertical, 0));
+	}
+}
