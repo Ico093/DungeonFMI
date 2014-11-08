b8440d044a87e79dc35c5e63694a25f8fd305f6c
FIlip Uzunov <filkata@gmail.com>
2014-11-08 02:04:14 +0200

    *fixed movement

--- Assets/Scripts/playerControllerScr.cs
+++ Assets/Scripts/playerControllerScr.cs
@@ -4,12 +4,9 @@ using System.Collections;
 public class playerControllerScr : MonoBehaviour {
 
 	public float movementSpeed;
-	public Object projectile;
-	public float projectileTimer;
 
 	float moveHorizontal;
 	float moveVertical;
-	projectileControllerScr projectileHelper;
 	
 	void Start () 
 	{
@@ -19,7 +16,6 @@ public class playerControllerScr : MonoBehaviour {
 	void Update () 
 	{
 		PlayerMovement ();
-		Shoot ();
 	}
 
 	void PlayerMovement()
@@ -27,21 +23,5 @@ public class playerControllerScr : MonoBehaviour {
 		moveHorizontal = Input.GetAxis ("Horizontal") ;
 		moveVertical = Input.GetAxis ("Vertical") ;
 		transform.Translate (new Vector3(moveHorizontal, moveVertical, 0).normalized * movementSpeed * Time.deltaTime);
-	}
-
-	void Shoot()
-	{
-		projectileTimer -= Time.deltaTime;
-		if (projectileTimer <= 0 || Input.GetKeyDown(KeyCode.Space)) {
-			projectile = Instantiate(projectile, transform.position, Quaternion.identity);
-			projectileHelper = GetComponent<projectileControllerScr>();
-			projectileHelper.SetTargetPosition(new Vector2(1, 0));
-			projectileTimer = 2;
-				}
-	}
-
-	void SetDirection()
-	{
-
 	}
 }
