52a7a14edeafaaae2b50d9f809c196d4b8cd0548
FIlip Uzunov <filkata@gmail.com>
2014-11-08 05:29:23 +0200

    -new outdoor bg

--- Assets/Scripts/playerControllerScr.cs
+++ Assets/Scripts/playerControllerScr.cs
@@ -9,12 +9,18 @@ public class playerControllerScr : MonoBehaviour {
 	float moveVertical;
 
 	SpriteRenderer sr;
-	public Sprite[] states;
+	public Sprite top;
+	public Sprite bottom;
+	public Sprite left;
+	public Sprite right;
+	public Sprite topLeft;
+	public Sprite topRight;
+	public Sprite bottomLeft;
+	public Sprite bottomRight;
 	
 	void Start () 
 	{
 		sr = GetComponent<SpriteRenderer>();
-		states = Resources.LoadAll<Sprite>("brain");
 	}
 
 	void FixedUpdate () 
@@ -28,45 +34,41 @@ public class playerControllerScr : MonoBehaviour {
 		moveVertical = Input.GetAxis ("Vertical") ;
 		Vector2 direction = new Vector2 ();	
 		transform.Translate (new Vector3(moveHorizontal, moveVertical, 0).normalized * movementSpeed * Time.deltaTime);
-
 		if (moveHorizontal == 0 && moveVertical > 0) {
+			Debug.Log("TOP");
 			direction.x = 0;
 			direction.y = 1;
-			sr.sprite = states[3];
+			sr.sprite = top;
 		} else if (moveHorizontal == 0 && moveVertical < 0) {
 			direction.x = 0;
 			direction.y = -1;
-			sr.sprite = states[7];
+			sr.sprite = bottom;
 		} else if (moveHorizontal > 0 && moveVertical == 0) {
 			direction.x = 1;
 			direction.y = 0;
-			sr.sprite = states[5];
+			sr.sprite = right;
 		} else if (moveHorizontal < 0 && moveVertical == 0) {
-			sr.sprite = states[1];
+			sr.sprite = left;
 			direction.x = -1;
 			direction.y = 0;
 		} else if (moveHorizontal > 0 && moveVertical > 0) {
 			direction.x = 1;
 			direction.y = 1;
-			sr.sprite = states[4];
+			sr.sprite = topRight;
 		} else if (moveHorizontal < 0 && moveVertical > 0) {
 			direction.x = -1;
 			direction.y = 1;
-			sr.sprite = states[2];
+			sr.sprite = topLeft;
 		} else if (moveHorizontal > 0 && moveVertical < 0) {
 			direction.x = 1;
 			direction.y = -1;
-			sr.sprite = states[6];
+			sr.sprite = bottomRight;
 		} else if (moveHorizontal < 0 && moveVertical < 0) {
 			direction.x = -1;
 			direction.y = -1;
-			sr.sprite = states[0];
+			sr.sprite = bottomLeft;
 		} else { 
+			Debug.Log(" default " );
 		}
-	}
-
-	void OnTriggerEnter(Collider other)
-	{
-		Application.LoadLevel ("Dungeon");
 	}
 }
