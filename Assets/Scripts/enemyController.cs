6c28557381211b46296eb488ce5fa340da60c562
Viktor Markov <vikata3@gmail.com>
2014-11-08 04:26:08 +0200

    small changes in implementation of sprites

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -2,22 +2,43 @@
 using System.Collections;
 
 public class enemyController : MonoBehaviour {
+	SpriteRenderer sr;
 
 	public float chaseSpeed = 0.002f;                           // The nav mesh agent's speed when chasing.
 
 	public GameObject player;    // Reference to the last global sighting of the player.
-
-	
+	public Sprite[] states;
+		
 	void Start () {
-		player=GameObject.Find("player");
-		}
+		sr = GetComponent<SpriteRenderer>();
+		player = GameObject.Find("player");
+		states = Resources.LoadAll<Sprite>("zombie1");
+	}
 	
 	void Update ()
-	{
-		
-		Vector3 player_position=player.transform.position;
+	{	
+		Vector3 player_position = player.transform.position;
 		transform.position -= ((transform.position - player_position).normalized * chaseSpeed * Time.deltaTime);
-	
+		double moveHorizontal = (transform.position - player_position).normalized.x;
+		double moveVertical = (transform.position - player_position).normalized.y;
+		if (moveHorizontal == 0 && moveVertical > 0) {
+			sr.sprite = states[3];
+		} else if (moveHorizontal == 0 && moveVertical < 0) {
+			sr.sprite = states[2];
+		} else if (moveHorizontal > 0 && moveVertical == 0) {
+			sr.sprite = states[5];
+		} else if (moveHorizontal < 0 && moveVertical == 0) {
+			sr.sprite = states[1];
+		} else if (moveHorizontal > 0 && moveVertical > 0) {
+			sr.sprite = states[4];
+		} else if (moveHorizontal < 0 && moveVertical > 0) {
+			sr.sprite = states[2];
+		} else if (moveHorizontal > 0 && moveVertical < 0) {
+			sr.sprite = states[6];
+		} else if (moveHorizontal < 0 && moveVertical < 0) {
+			sr.sprite = states[0];
+		} else { 
+		}
 	}
 
 
