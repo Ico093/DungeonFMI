54ef68af15eea994d584ecea81d5d4ffe4a28b6d
Viktor Markov <vikata3@gmail.com>
2014-11-08 02:05:47 +0200

    Movement details

--- Assets/Scripts/playerControllerScr.cs
+++ Assets/Scripts/playerControllerScr.cs
@@ -10,10 +10,20 @@ public class playerControllerScr : MonoBehaviour {
 	float moveHorizontal;
 	float moveVertical;
 	projectileControllerScr projectileHelper;
+
+	SpriteRenderer sr;
+	public Sprite top;
+	public Sprite bottom;
+	public Sprite left;
+	public Sprite right;
+	public Sprite topLeft;
+	public Sprite topRight;
+	public Sprite bottomLeft;
+	public Sprite bottomRight;
 	
 	void Start () 
 	{
-
+		sr = GetComponent<SpriteRenderer>();
 	}
 
 	void Update () 
@@ -26,7 +36,44 @@ public class playerControllerScr : MonoBehaviour {
 	{
 		moveHorizontal = Input.GetAxis ("Horizontal") ;
 		moveVertical = Input.GetAxis ("Vertical") ;
+		Vector2 direction = new Vector2 ();	
 		transform.Translate (new Vector3(moveHorizontal, moveVertical, 0).normalized * movementSpeed * Time.deltaTime);
+		if (moveHorizontal == 0 && moveVertical > 0) {
+			Debug.Log("TOP");
+			direction.x = 0;
+			direction.y = 1;
+			sr.sprite = top;
+		} else if (moveHorizontal == 0 && moveVertical < 0) {
+			direction.x = 0;
+			direction.y = -1;
+			sr.sprite = bottom;
+		} else if (moveHorizontal > 0 && moveVertical == 0) {
+			direction.x = 1;
+			direction.y = 0;
+			sr.sprite = right;
+		} else if (moveHorizontal < 0 && moveVertical == 0) {
+			sr.sprite = left;
+			direction.x = -1;
+			direction.y = 0;
+		} else if (moveHorizontal > 0 && moveVertical > 0) {
+			direction.x = 1;
+			direction.y = 1;
+			sr.sprite = topRight;
+		} else if (moveHorizontal < 0 && moveVertical > 0) {
+			direction.x = -1;
+			direction.y = 1;
+			sr.sprite = topLeft;
+		} else if (moveHorizontal > 0 && moveVertical < 0) {
+			direction.x = 1;
+			direction.y = -1;
+			sr.sprite = bottomRight;
+		} else if (moveHorizontal < 0 && moveVertical < 0) {
+			direction.x = -1;
+			direction.y = -1;
+			sr.sprite = bottomLeft;
+		} else { 
+			Debug.Log(" default " );
+		}
 	}
 
 	void Shoot()
@@ -37,7 +84,7 @@ public class playerControllerScr : MonoBehaviour {
 			projectileHelper = GetComponent<projectileControllerScr>();
 			projectileHelper.SetTargetPosition(new Vector2(1, 0));
 			projectileTimer = 2;
-				}
+		}
 	}
 
 	void SetDirection()
