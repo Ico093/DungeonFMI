1737562b49abc77ba26215a49e6146bd8fd52d9b
Viktor Markov <vikata3@gmail.com>
2014-11-09 16:39:21 +0200

    Added graffiti

---
+++ Assets/GraffitiTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraffitiTrigger : MonoBehaviour 
+{
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		Debug.Log ("trigger");
+		Application.LoadLevel ("breakout"); 
+	}
+}
