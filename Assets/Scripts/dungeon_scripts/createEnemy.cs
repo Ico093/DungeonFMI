c732cf6860b2f08d5bdc9acad0d7d95dce031c90
Shavarsh Movsesyan <naps250@gmail.com>
2014-11-09 08:23:20 +0200

    last night's changes

--- Assets/Scripts/dungeon_scripts/createEnemy.cs
+++ Assets/Scripts/dungeon_scripts/createEnemy.cs
@@ -5,18 +5,20 @@ public class createEnemy : MonoBehaviour {
 
 	public GameObject zombie;
 	public GameObject fireZombie;
+	GameObject player;
+	public GameObject floor;
 	public float speed = 10f;
 	int count=0;
 	// Use this for initialization
 	void Start () {
-		
+		player = GameObject.Find ("player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		int rand = Random.Range (1, 1000);
 		if (rand > 990 ) {
-			Vector3 tempZombieVector=new Vector3 (Random.Range (-30, -10), 0f, 0f);
+			Vector3 tempZombieVector=new Vector3 (player.transform.position.x+Random.Range(-2,2), player.transform.position.y+Random.Range(1,floor.renderer.bounds.size.y)/2, 0f);
 			var tempZombie = (GameObject)Instantiate (Random.Range (1, 10) > 5 ? fireZombie : zombie,tempZombieVector , Quaternion.identity);
 			count++;
 				}
---
+++ Assets/exetendpath.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exetendpath : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject hole1;
+	public GameObject hole2;
+	void FixedUpdate()
+	{
+
+		float widthOfObject = renderer.bounds.size.y*3;
+		Vector3 pos = transform.position;
+		if (player.transform.position.y > pos.y+(renderer.bounds.size.y)) {
+						pos.y += widthOfObject;
+						transform.position = pos;
+						Vector3 vector=new Vector3(Random.Range(1,renderer.bounds.size.x)-renderer.bounds.size.x/2,Random.Range(10,renderer.bounds.size.y)+renderer.bounds.size.y/2,0);
+						Instantiate (hole1, transform.position-vector ,Quaternion.identity);
+				}
+	}
+}
